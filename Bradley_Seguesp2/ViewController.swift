e706c3a6f8506ae1132a793b690efde57f7dc6d4
Bradley <bradleypetit5@gmail.com>
2021-04-18 17:46:10 -0400

    OK

--- Bradley_Seguesp2/ViewController.swift
+++ Bradley_Seguesp2/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var segueSwitch: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
 
-
+  
+    @IBAction func goToRed(_ sender: Any) {
+        if segueSwitch.isOn {
+        performSegue(withIdentifier: "greenToRed", sender: nil)
+        }
+    }
+    
+    @IBAction func goToBlue(_ sender: Any) {
+        if segueSwitch.isOn {
+        performSegue(withIdentifier: "greenToBlue", sender: nil)
+        }
+    }
+    
+    
 }
 
